519306487c0dc40edee02eb4be38ac957e224cfa
vvendorrr <vandenkeijbusfamke@gmail.com>
2026-03-04 09:23:13 +0100

    Update index.php

--- resources/views/meldingen/index.php
+++ resources/views/meldingen/index.php
@@ -20,6 +20,32 @@
             echo "<div class='msg'>" . $_GET['msg'] . "</div>";
         } ?>
 
+        <?php
+            //Query uitvoeren:
+            require_once '../../../config/conn.php';
+            $query = "SELECT * FROM meldingen";
+            $statement = $conn->prepare($query);
+            $statement->execute();
+            $meldingen = $statement->fetchAll(PDO::FETCH_ASSOC);
+        ?>
+
+        <table>
+            <tr>
+                <th>Attractie</th>
+                <th>Type</th>
+                <th>Melder</th>
+                <th>Overige info</th>
+            </tr>
+            <?php foreach($meldingen as $melding): ?>
+                <tr>
+                    <td><?php echo $melding['attractie']; ?></td>
+                    <td><?php echo $melding['type']; ?></td>
+                    <td><?php echo $melding['melder']; ?></td>
+                    <td><?php echo $melding['overig']; ?></td>
+                </tr>
+            <?php endforeach; ?>
+        </table>
+
         <div style="height: 300px; background: #ededed; display: flex; justify-content: center; align-items: center; color: #666666;">(hier komen de storingsmeldingen)</div>
     </div>
 
